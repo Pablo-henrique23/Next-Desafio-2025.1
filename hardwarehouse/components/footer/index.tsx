eaf7fc6e268a0c504803112c79e5847e25a0835c
Pablo Henrique <pablohenriquesilva26@gmail.com>
2025-03-02 16:02:27 -0300

    coisa pouca, mudei a navbar e o footer

--- hardwarehouse/components/footer/index.tsx
+++ hardwarehouse/components/footer/index.tsx
@@ -3,61 +3,71 @@ import Link from "next/link"
 export default function Footer(){
     return(
         <footer className="">
-            <div className="flex gap-[40px] bg-[#B3C9E2] py-10 item-center justify-center">
-
-                <div className="hidden md:flex text-[18px] items-center flex item-center justify-center font-semibold mx-10">
-                    <p>R. Falsa, 0 - Null<br></br>
-                    Juiz de Fora - MG, <br></br>
-                    12345-069
-                    </p>
+            <div className="flex w-full bg-[#B3C9E2] py-5 item-center text-center justify-between md:px-5">
+                <div className="flex flex-col ml-3 gap-[20px] items-left justify-left">
+                    <p className="text-[20px] font-semibold text-left w-36">Siga-nos!</p>
+                    <Link href="/" className="flex items-center gap-[10px] text-[16px] text-black/75 font-semibold hover:scale-105 transition-all duration-300 ease-in-out">
+                        <Image
+                            src={'/assets/instagram.png'}
+                            alt="Instagram"
+                            width={1920}
+                            height={1080}
+                            className="flex size-7 rounded-full"/>
+                        Instagram
+                    </Link>
+                    <Link href="/" className="flex items-center gap-[10px] text-[16px] text-black/75 font-semibold hover:scale-105 transition-all duration-300 ease-in-out">
+                        <Image
+                            src={'/assets/facebook.png'}
+                            alt="Facebook"
+                            width={1920}
+                            height={1080}
+                            className="flex size-7 rounded-full"/>
+                        Facebook
+                    </Link>
+                    <Link href="/" className="flex items-center gap-[10px] text-[16px] text-black/75 font-semibold hover:scale-105 transition-all duration-300 ease-in-out">
+                        <Image
+                            src={'/assets/linkedin.png'}
+                            alt="Linkedin"
+                            width={1920}
+                            height={1080}
+                            className="flex size-7 rounded-full"/>
+                        Linkedin
+                    </Link>
+                    <div className="hidden md:flex flex-col gap-4 text-left">
+                        <p className="text-[20px] font-semibold">Formas de pagamento</p>
+                        <Image
+                            src={'/assets/pagamentos.png'}
+                            alt="Formas de pagamento"
+                            width={1920}
+                            height={1080}
+                            className="flex size-full"
+                        />   
+                    </div>
                 </div>
-                <div className="items-center flex mx-auto">
+                <div className="items-center flex">
                     <Image
-                        className="md:size-[180px]"
+                        className="size-sm md:size-72"
                         alt="Logo"
                         src={'/assets/no_bg_black_logo.png'}
-                        height={200}
-                        width={200}
-                        />
-
+                        height={1920}
+                        width={1080}
+                    />
                 </div>
-                <div className="pr-5">
-                    <p className="text-[18px] font-semibold">Nossas redes sociais</p>
-                    <div className="flex mt-3 mb-3 gap-[20px] items-center justify-left">
-                        <Link href="https://instagram.com/_pablo_henrique_23" target="_blank">
-                            <Image
-                                className="size-[30px] md:size-[35px]"
-                                alt="Instagram"
-                                src={"/assets/instagram_black.png"}
-                                height={35}
-                                width={35}/>
-                        </Link>
-                        <Link href="/">
-                            <Image
-                                className="size-[30px] md:size-[35px]"
-                                alt="Facebook"
-                                src={"/assets/facebook_black.png"}
-                                height={35}
-                                width={35}/>
-                        </Link>
-                        <Link href="/">
-                            <Image
-                                className="size-[30px] md:size-[35px]"
-                                alt="Twitter"
-                                src={"/assets/twitter.png"}
-                                height={35}
-                                width={35}/>
-                        </Link>
-                    </div>
-                    <p className="text-[18px] font-semibold">Formas de pagamento</p>
-                    <Image
-                        className="mt-3"
-                        alt="Formas de pagamento"
-                        src={"/assets/pagamentos.png"}
-                        height={300}
-                        width={300}
-                        />
-
+                <div className="hidden mr-3 md:flex gap-4 md:flex-col flex text-right">
+                    <p className="text-[20px] font-semibold">
+                        Fale conosco!
+                    </p>
+                    <p className="text-[18px] text-black/80">
+                        manager@hardwarehouse.com
+                    </p>
+                    <p className="text-[18px] text-black/80">
+                        (31) 94002-8922
+                    </p>
+                    <p id="endereço" className="text-[18px] text-black/80">
+                        R. Falsa, 0 - Null<br></br>
+                        Juiz de Fora - MG, <br></br>
+                        12345-069
+                    </p>
                 </div>
             </div>
             {/* <div className="bg-[#075494] items-center text-center">
--- hardwarehouse/components/header/index.tsx
+++ hardwarehouse/components/header/index.tsx
@@ -5,94 +5,98 @@ import Link from "next/link"
 import { useEffect, useState } from "react"
 
 const links = [
+    {href: '/', label: 'Home'},
     {href: '/products', label: 'Produtos'},
-    {href:'/gpus', label: 'GPUs'},
-    {href:'/cpus', label: 'CPUs'},
-    {href:'/cases', label: 'Gabinetes'},
-    {href:'/memories', label: 'Memórias'},
+    {href: '/contact', label: 'Contato'},
+
 ]
 
 export default function Header() {
-    const [visibleLinks, setVisibleLinks] = useState(links);
-    const mobile = ["Produtos", "Gabinetes", "Memórias"]
+    const visibleLinks = links;
+    // const [visibleLinks, setVisibleLinks] = useState(links);
+    // const mobile = ["Produtos", "Gabinetes", "Memórias"]
 
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth < 768) {
-                setVisibleLinks(links.filter(
-                    (link) => mobile.includes(link.label)
-                )); // Exibe apenas os três primeiros
-            } else {
-                setVisibleLinks(links);
-            }
-        };
+    // useEffect(() => {
+    //     const handleResize = () => {
+    //         if (window.innerWidth < 768) {
+    //             setVisibleLinks(links.filter(
+    //                 (link) => mobile.includes(link.label)
+    //             )); // Exibe apenas os três primeiros
+    //         } else {
+    //             setVisibleLinks(links);
+    //         }
+    //     };
 
-        handleResize(); // Chama a função ao montar o componente
-        window.addEventListener("resize", handleResize);
+    //     handleResize(); // Chama a função ao montar o componente
+    //     window.addEventListener("resize", handleResize);
 
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    //     return () => window.removeEventListener("resize", handleResize);
+    // }, []);
 
     return (
         <header>
-            <nav className="justify-center">
+            <nav className="justify-center text-center items-center">
                 <div className="flex bg-[#B3C9E2] items-center justify-around flex-wrap w-full ">
                     <Link href="/" className="">
                         <Image
                             src={'/assets/no_bg_black_logo.png'}
                             alt="Logo"
-                            width={50}
-                            height={50}
-                            className="md:size-[80px]"/>
+                            width={1920}
+                            height={1080}
+                            className="size-[50px] md:size-[80px]"
+                        />
                     </Link>
 
-                    <form action="/" className="flex w-1/2 rounded-[8px] bg-white border border-black px-3 py-1">
+                    <form action="/" className="flex w-1/2 rounded-full bg-white border border-black px-3 py-1">
                         <input type="text"
                         placeholder="Buscar"
                         className="w-full text-black focus:outline-none focus:ring-0 caret-black" />
                         <button type="submit" className="">
                             <Image
-                            src={'/assets/lupa.png'}
-                            alt="Confirmar busca"
-                            width={30}
-                            height={30}
+                                src={'/assets/lupa.png'}
+                                alt="Confirmar busca"
+                                width={30}
+                                height={30}
                             />
                         </button>
 
                     </form>
-                    <div className="flex items-center justify-between gap-[10px] md:gap-[20px]">
-                        <Link href="/manage" className="">
-                            <Image
-                            alt="Manager"
-                            src={"/assets/manager.png"}
-                            width={20}
-                            height={20}
-                            className="md:size-[30px] hover:bg-[#f1f1f1] hover:rounded-[10px] hover:p-1 transition-all duration-300 easy-in-out "/>
-                        </Link>
+                    <div className="flex items-center text-center md:w-36 justify-between gap-[10px] md:gap-[20px]">
                         <Link href="/contact" className="">
                             <Image
-                            alt="Contatos"
-                            src={"/assets/contact-mail.png"}
-                            width={20}
-                            height={20}
-                            className="md:size-[30px] hover:bg-[#f1f1f1] hover:rounded-[10px] hover:p-1 transition-all duration-300 easy-in-out "/>
+                                alt="Contatos"
+                                src={"/assets/contact-mail.png"}
+                                width={1920}
+                                height={1080}
+                                className="size-[25px] md:size-[30px] hover:bg-[#f1f1f1] hover:rounded-[10px] hover:p-1 transition-all duration-300 easy-in-out "
+                            />
                         </Link>
                         <Link href="/user" className="">
                             <Image
-                            alt=""
-                            src={"/assets/user.png"}
-                            width={20}
-                            height={20}
-                            className="md:size-[30px] hover:bg-[#f1f1f1] hover:rounded-[10px] hover:p-1 transition-all duration-300 easy-in-out "/>
+                                alt=""
+                                src={"/assets/user.png"}
+                                width={1920}
+                                height={1080}
+                                className="size-[25px] md:size-[30px] hover:bg-[#f1f1f1] hover:rounded-[10px] hover:p-1 transition-all duration-300 easy-in-out "
+                            />
                         </Link>
                     </div>
                 </div>
-                <div className="flex w-full justify-around bg-[#dfdfdf] py-[4px]">
+                <div className="flex w-full justify-around items-center bg-[#dfdfdf] py-[4px]">
                     {visibleLinks.map((link, index) =>
                         <Link href={link.href} key={index} className="justify-between">
                             <span className="rounded-[8px] hover:bg-[#aaaaaa] hover:text-white hover:px-[2px] transition-all duration-300 easy-in-out">{link.label}</span>
                         </Link>
                     )}
+                    <Link href="/admin" className="">
+                        <Image
+                            alt="Manager"
+                            src={"/assets/manager.png"}
+                            width={20}
+                            height={20}
+                            className="md:size-[25px] hover:bg-[#f1f1f1] hover:rounded-[10px] hover:p-1 transition-all duration-300 easy-in-out "
+                        />
+                    </Link>
                 </div>
             </nav>
         </header>
