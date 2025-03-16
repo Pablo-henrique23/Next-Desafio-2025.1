200d72d834d9437f763afe6835610f7b752a9acd
Pablo Henrique <pablohenriquesilva26@gmail.com>
2025-03-16 15:02:36 -0300

    detalhes

--- hardwarehouse/app/(home)/forgot/page.tsx
+++ hardwarehouse/app/(home)/forgot/page.tsx
@@ -1,7 +1,8 @@
 export default function Page() {
 	return (
-		<div>
-			<h1>Indisponível</h1>
+		<div className='flex flex-col text-center items-center justify-center w-full my-3'>
+			<h1 className='font-semibold'>Indisponível</h1>
+			<a href='/login' className='underline hover:scale-105'>Voltar</a>
 		</div>	
 	)
 }
